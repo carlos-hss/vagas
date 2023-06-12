0589e94f93f253d53830b87e34f85887811bca30
Carlos Henrique <carloshenriquesantanas19@gmail.com>
2023-06-12 11:35:08 -0300

    fix: improve the code to return only one data and errors if necessary

--- teste1.js
+++ teste1.js
@@ -1,24 +1,30 @@
-var data =  require("./fakeData");
+let data = require("./fakeData");
 
-const getUser = ( req, res, next ) => {
-    
-    var name =  req.query.name;
+const getUser = (req, res, next) => {
+  // Eu alteraria para a função fazer a busca por cpf, pelo fato de ser uma chave única, mas deixei o nome pois creio que seja a intenção do código.
 
-    for(let i = 0; i < data.length;  i++) {
-        if(i.name == name) {
-            res.send(data[i]);
-        }
+  let name = req.query.name;
+
+  for (const user of data) {
+    if (user.name == name) {
+      res.send(user);
+      return;
     }
+  }
 
+  return res.send("User not founded");
 };
 
-const getUsers = ( req, res, next ) => {
-    
+const getUsers = (req, res, next) => {
+  if (data.length > 0) {
     res.send(data);
-    
+    return;
+  }
+
+  return res.send("No users registered");
 };
 
 module.exports = {
-    getUser,
-    getUsers
+  getUser,
+  getUsers,
 };
